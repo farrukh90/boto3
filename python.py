ccc57f6718eeb27d41e407a8481da92d36969227
farrukh90 <farrukhsadykov@gmail.com>
2019-07-20 22:53:41 -0500

    added

--- python.py
+++ python.py
@@ -8,4 +8,6 @@ for bucket in s3.buckets.all():
 
 data = open('test', 'rb')
 
+# Key is the name of the file that  goes into s3, and the data is the content
+# monolitapp is the bucketname
 s3.Bucket('monolithapp').put_object(Key='test_2', Body=data)
